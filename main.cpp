8d8402a43870fd6586e33df50dd9c5f8e5694711
Holger Stenger <holger.stenger@gmx.de>
2013-04-20 23:56:28 +0200

    Added component description.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,13 @@
 
 DECLARE_COMPONENT_VERSION(
 "Multi-formatting Tech Demo",
-"1.0 alpha 2013-03-21",
+"1.0 alpha 2013-04-21",
+"This component is a tech demo. As such it may not be as stable as a production quality release.\n"
+"The component demonstrates a title formatting technique which allows using arbitrary functions on "
+"the individual values of multi-value fields. "
+"For example, the expression \"$left(%<artist>%,1)|%<artist>%\" should work like most users expect."
+"Moreover, it will produce the same result as \"$left($put(A,%<artist>%),1)|$get(A)\".\n"
+"\n"
 "Copyright (C) 2008-2013 Holger Stenger"
 )
 
